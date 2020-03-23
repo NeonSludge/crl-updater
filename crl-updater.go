80337e2e4afb976043bb7191b0eef56d09dffc2f
Kirill Buev <kirill.buev@pm.me>
2020-03-23 23:10:54 +0300

    CRL validation, atomic rename, error wrapping

--- crl-updater.go
+++ crl-updater.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/sha256"
 	"flag"
-	"fmt"
+	"hash"
 	"io"
 	"io/ioutil"
 	"log"
@@ -11,10 +13,19 @@ import (
 	"os"
 	"time"
 
+	"github.com/google/renameio"
+	"github.com/pkg/errors"
 	"github.com/robfig/cron/v3"
 	"gopkg.in/yaml.v3"
 )
 
+var (
+	X509CRLPEMHeader string = "-----BEGIN X509 CRL-----"
+	X509CRLDERMagic1 []byte = []byte{48, 130}
+	X509CRLDERMagic2 []byte = []byte{48, 131}
+)
+
+// CRL update job definition
 type CRLJob struct {
 	ID cron.EntryID
 
@@ -25,64 +36,109 @@ type CRLJob struct {
 	TimeoutDuration time.Duration
 }
 
+// Runs this CRL update job
 func (j *CRLJob) Run() {
-	tmp, err := os.Create(fmt.Sprintf("%s.tmp", j.Destination))
+	tempFile, err := renameio.TempFile("", j.Destination)
 	if err != nil {
-		log.Printf("failed to create a temporary file: %v", err)
+		log.Printf("[%v] [%s]: failed to create a temporary file: %v", j.ID, j.Destination, err)
 		return
 	}
-	defer os.Remove(tmp.Name())
-	defer tmp.Close()
+	defer tempFile.Cleanup()
 
-	w := bufio.NewWriter(tmp)
-	if err := downloadCRL(j.Source, w, j.TimeoutDuration); err != nil {
-		log.Printf("failed to download CRL: %v", err)
-		return
-	}
+	tempWriter := bufio.NewWriter(tempFile)
+	tempHash := sha256.New()
 
-	if err := w.Flush(); err != nil {
-		log.Printf("failed to write to temporary file: %v", err)
+	if err := downloadCRL(j.Source, tempWriter, tempHash, j.TimeoutDuration); err != nil {
+		log.Printf("[%v] [%s]: failed to download CRL: %v", j.ID, j.Destination, err)
 		return
 	}
 
-	tmp.Close()
-	if err = os.Rename(tmp.Name(), j.Destination); err != nil {
-		log.Printf("failed to replace existing CRL file: %v", err)
+	destFile, err := os.Open(j.Destination)
+	if err == nil {
+		defer destFile.Close()
+
+		destHash := sha256.New()
+		if _, err := io.Copy(destHash, bufio.NewReader(destFile)); err != nil {
+			log.Printf("[%v] [%s]: failed to compare CRL files: %v", j.ID, j.Destination, err)
+			return
+		}
+
+		if bytes.Equal(tempHash.Sum(nil), destHash.Sum(nil)) {
+			log.Printf("[%v] [%s]: CRL source did not change", j.ID, j.Destination)
+			return
+		}
+	}
+
+	if err := tempFile.CloseAtomicallyReplace(); err != nil {
+		log.Printf("[%v] [%s]: failed to replace existing CRL file: %v", j.ID, j.Destination, err)
 		return
 	}
+
+	log.Printf("[%v] [%s]: updated target CRL file", j.ID, j.Destination)
 }
 
+// Config file structure
 type Config struct {
 	CRLJobs []*CRLJob `yaml:"jobs"`
 }
 
+// Unmarshal YAML in the config file
 func makeConfig(r *bufio.Reader) (*Config, error) {
 	b, err := ioutil.ReadAll(r)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "config file loading failed")
 	}
 
 	cfg := &Config{}
 	err = yaml.Unmarshal(b, cfg)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "config file unmarshalling failed")
 	}
 
 	return cfg, nil
 }
 
 // Download CRL file from the specified location and write it to the specified Writer
-func downloadCRL(src string, dest *bufio.Writer, timeout time.Duration) error {
+func downloadCRL(src string, w *bufio.Writer, h hash.Hash, timeout time.Duration) error {
 	c := &http.Client{Timeout: timeout}
 	r, err := c.Get(src)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "http request failed")
 	}
 	defer r.Body.Close()
 
-	_, err = io.Copy(dest, r.Body)
-	if err != nil {
-		return err
+	dest := io.MultiWriter(w, h)
+
+	head := make([]byte, 24)
+	if _, err := io.ReadFull(r.Body, head); err != nil {
+		return errors.Wrap(err, "head read failed")
+	}
+
+	if err := validateHead(head); err != nil {
+		return errors.Wrap(err, "head validation failed")
+	}
+
+	if _, err = io.Copy(dest, bytes.NewReader(head)); err != nil {
+		return errors.Wrap(err, "head copy failed")
+	}
+
+	if _, err := io.Copy(dest, r.Body); err != nil {
+		return errors.Wrap(err, "body copy failed")
+	}
+
+	if err := w.Flush(); err != nil {
+		return errors.Wrap(err, "temp writer flush failed")
+	}
+
+	return nil
+}
+
+// Validate received response fragment
+func validateHead(b []byte) error {
+	if string(b) != X509CRLPEMHeader {
+		if !(bytes.Equal(b[:2], X509CRLDERMagic1) || bytes.Equal(b[:2], X509CRLDERMagic2)) {
+			return errors.New("not a PEM or DER encoded file")
+		}
 	}
 
 	return nil
@@ -93,45 +149,46 @@ func main() {
 	cfgPath := flag.String("cfg", "/etc/crl-updater.yaml", "path to a config file in YAML format")
 	flag.Parse()
 
-	// Config file parsing
 	cfgFile, err := os.Open(*cfgPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(errors.Wrap(err, "config file opening failed"))
 	}
 
+	// Unmarshal config file
 	cfg, err := makeConfig(bufio.NewReader(cfgFile))
 	if err != nil {
-		log.Fatalf("failed to parse configuration: %v", err)
+		log.Fatal(errors.Wrap(err, "config file parsing failed"))
 	}
 	cfgFile.Close()
 
 	sched := cron.New()
 	jobs := cfg.CRLJobs
+	// Iterate on job list
 	for _, job := range jobs {
 		if job.Source == "" || job.Destination == "" {
-			log.Printf("empty source or destination, skipping job: (%s) %s -> %s", job.Schedule, job.Source, job.Destination)
+			log.Printf("empty source (%s) or destination (%s), skipping job", job.Source, job.Destination)
 			continue
 		}
-		// Default schedule
+		// Default schedule (if not specified/invalid)
 		if _, err := cron.ParseStandard(job.Schedule); err != nil {
-			log.Printf("failed to parse job schedule (%s): %v", job.Schedule, err)
+			log.Printf("[%s]: failed to parse job schedule, assuming default: %v", job.Destination, err)
 			job.Schedule = "@hourly"
 		}
-		// Default timeout
+		// Default timeout (if not specified/invalid)
 		if job.TimeoutDuration, err = time.ParseDuration(job.TimeoutHuman); err != nil {
-			log.Printf("failed to parse job timeout (%s): %v", job.TimeoutHuman, err)
+			log.Printf("[%s]: failed to parse job timeout, assuming default: %v", job.Destination, err)
 			job.TimeoutDuration = time.Second * 60
 		}
 		// Add job to scheduler
 		id, err := sched.AddJob(job.Schedule, job)
 		if err != nil {
-			log.Printf("failed to add CRL update job: %v", err)
+			log.Printf("[%s]: failed to add CRL update job: %v", job.Destination, err)
 			continue
 		}
-		// Store job ID
 		job.ID = id
-		log.Printf("added CRL update job: [%v] (%s) %s -> %s", job.ID, job.Schedule, job.Source, job.Destination)
+		log.Printf("[%v] [%s]: added CRL update job", job.ID, job.Destination)
 	}
+	// Run jobs
 	sched.Start()
 
 	for {
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/NeonSludge/crl-updater
 go 1.14
 
 require (
+	github.com/google/renameio v0.1.0
+	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron/v3 v3.0.1
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
